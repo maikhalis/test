86cec1ff09b0f27b7714d32382e47d33a21fca30
maikhalis <astynian@gmail.com>
2015-05-05 17:57:31 +0200

    nuget update and co

---
+++ MvcMusicStore/MvcMusicStore/Models/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcMusicStore.Models
+{
+    public class Genre
+    {
+        public virtual int GenreId { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public List<Album> Albums { get; set; }
+    }
+}
---
+++ MvcMusicStore/MvcMusicStore/Models/MusicStoreDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class MusicStoreDbInitializer : DropCreateDatabaseAlways<MusicStoreDB>
+    {
+        protected override void Seed(MusicStoreDB context)
+        {
+            context.Artists.AddOrUpdate(new Artist { Name = "Jimi Hendrix" });
+            context.Genres.AddOrUpdate(new Genre { Name = "Jazz" });
+            context.Albums.AddOrUpdate(new Album
+                {
+                    Artist = new Artist {  Name = "Rush"},
+                    Genre = new Genre { Name = "Jazz"},
+                    Price = 9.99m,
+                    Title = "Caravan"
+                });
+            base.Seed(context);
+        }
+    }
+}
